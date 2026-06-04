007db86d0c9862fbef0ffb19bb78386c0f6f57fa
penpalac <penpalac@student.42madrid.com>
2026-06-04 16:22:27 +0200

    4K works better now, added some new breakpoints for tablets. Need to check login/register pages too User is still under development

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,25 +6,15 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function Home ()
 {
-	console.log(window.innerWidth, window.innerHeight, window.devicePixelRatio);
-	console.log(getComputedStyle(document.documentElement).fontSize, document.documentElement.clientWidth);
-	console.log('>=1536px?', window.matchMedia('(min-width:1536px)').matches);
-	console.log('>=1920px?', window.matchMedia('(min-width:1920px)').matches);
     return (
 		<div className="h-screen relative">
 				<div className="relative w-full h-1/3 flex items-center justify-center border-b-8 border-b-slate-200 overflow-hidden">
 					{/*Banner*/}
 					<div className="absolute inset-0 overflow-hidden bg-slate-200">
-						<div className="flex w-screen h-full gap-4">
-							<div className="w-1/3 h-full marquee-item bg-[url(/game01.jpg)]">
-								Item 1
-							</div>
-							<div className="w-1/3 h-full marquee-item bg-[url(/game02.png)]">
-								Item 2
-							</div>
-							<div className="w-1/3 h-full marquee-item bg-[url(/game03.jpg)]">
-								Item 3
-							</div>
+						<div className="flex w-screen h-full gap-0 lg:gap-4">
+							<div className="w-1/3 h-full marquee-item bg-[url(/game01.jpg)]" aria-hidden="true"></div>
+							<div className="w-1/3 h-full marquee-item bg-[url(/game02.png)]" aria-hidden="true"></div>
+							<div className="w-1/3 h-full marquee-item bg-[url(/game03.jpg)]" aria-hidden="true"></div>
 						</div>
 					</div>
 					{/*Black screen*/}
@@ -33,18 +23,18 @@ function Home ()
 					</div>
 					{/*Title*/}
 					<div className="relative z-20 font-pressstart">
-						<p className="text-2xl lg:text-8xl text-slate-100 text-shadow-lg text-shadow-slate-400">FT_TRANSCENDENCE</p>
+						<p className="text-xl sm:text-3xl lg:text-6xl text-slate-100 text-shadow-lg text-shadow-slate-400">FT_TRANSCENDENCE</p>
 					</div>
 				</div>
-			{/* Background and descrition */}
-			<div className="relative w-full bg-linear-to-t from-(--gradient-dark) to-(--gradient-light) overflow-auto pb-70 md:pb-auto">
-				<div className="absolute inset-0 bg-[url(/arcadePatternRepeat.png)] bg-auto animate-diagonal opacity-10 pointer-events-none z-0" aria-hidden="true"></div>
-				<div className="relative w-full p-6 lg:p-12 flex flex-col items-center justify-center">
+			{/* Background and descrition */} {/* In mobile having h-2/3 and h-screen is perfect bu */}
+			<div className="relative w-full h-2/3 bg-linear-to-t from-(--gradient-dark) to-(--gradient-light) overflow-auto">
+				<div className="absolute h-screen sm:h-full inset-0 bg-[url(/arcadePatternRepeat.png)] bg-auto animate-diagonal opacity-10 pointer-events-none z-0" aria-hidden="true"></div>
+				<div className="relative w-full p-6 lg:p-12 sm:gap-8 flex flex-col items-center justify-center">
 					<div className=" text-slate-300 text-center text-lg lg:text-3xl font-aldrich wrap-normal">
-						<p>[DESCRIPTION EXAMPLE]</p>
-						<p>A self-hosted minigame webpage, made as part of the 42 curriculum</p>
+						<p>[FT_TRANSCENDENCE] is a multiplayer minigames platform where users can compete against each other in real-time. 
+							This project is part of the 42 curriculum and demonstrates full-stack web development.</p>
 					</div>
-					<div className="p-6 gap-4 lg:gap-8 flex flex-col md:flex-row">
+					<div className="p-6 gap-4 lg:gap-8 flex flex-col sm:flex-row">
 						<Link className="customButton lg:p-6 lg:text-2xl transition ease-out-4 bg-linear-to-t from-amber-500 to-amber-300" to="/login">
 							<p>Login</p>
 						</Link>
@@ -54,9 +44,10 @@ function Home ()
 					</div>
 				</div>
 				{/*Credits*/}
-				<div className="relative w-full flex flex-col items-center justify-center">
+				<div className="relative sm:absolute w-full py-6 flex flex-col items-center justify-center inset-x-0 bottom-0">
 					<p className="text-lg text-slate-200 opacity-80 font-bold font-aldrich">MADE BY</p>
 					<div className="p-4 gap-2 w-full lg:w-4/5 2xl:w-3/5 flex lg:flex-row flex-wrap justify-around items-center">
+					{/* Change div so it is img + name + role */}
 						<div className="credit-icon bg-[url(/icon01.jpeg)]"></div>
 						<div className="credit-icon bg-[url(/icon02.jpeg)]"></div>
 						<div className="credit-icon bg-[url(/icon03.jpeg)]"></div>
