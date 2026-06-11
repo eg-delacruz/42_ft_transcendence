91d0e4b313b9c5004de6a4891bfcdb05261c5138
penpalac <penpalac@student.42madrid.com>
2026-06-11 17:37:26 +0200

    Fixed the images, it was an obsolete filter. Also fixed the background animation getting cut when the content was scrollable

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,6 +9,7 @@ function Home ()
     return (
 		<div className="h-screen relative flex flex-col">
 				<div className="relative w-full h-1/3 flex items-center justify-center border-b-8 border-b-slate-200 overflow-hidden">
+
 					{/*Banner*/}
 					<div className="absolute inset-0 overflow-hidden bg-slate-200">
 						<div className="flex w-screen h-full gap-0 lg:gap-4">
@@ -26,9 +27,10 @@ function Home ()
 						<p className="text-xl sm:text-3xl lg:text-6xl text-slate-100 text-shadow-lg text-shadow-slate-400">FT_TRANSCENDENCE</p>
 					</div>
 				</div>
-			{/* Background and descrition */} {/* In mobile having h-2/3 and h-screen is perfect bu */}
-			<div className="relative w-full flex-1 bg-linear-to-t from-(--gradient-dark) to-(--gradient-light) overflow-scroll">
-				<div className="absolute h-screen sm:h-full inset-0 bg-[url(/arcadePatternRepeat.png)] bg-auto animate-diagonal opacity-10 pointer-events-none z-0" aria-hidden="true"></div>
+
+			{/* Background and description */}
+			<div className="relative w-full h-auto flex-1 bg-linear-to-t from-(--gradient-dark) to-(--gradient-light) overflow-scroll">
+				<div className="fixed h-screen w-screen inset-0 bg-[url(/arcadePatternRepeat.png)] bg-auto animate-diagonal opacity-10 pointer-events-none z-0" aria-hidden="true"></div>
 				<div className="relative w-full p-6 lg:p-12 sm:gap-8 flex flex-col items-center justify-center overflow-scroll">
 					<div className=" text-slate-300 text-center text-lg lg:text-3xl font-aldrich wrap-normal">
 						<p>[FT_TRANSCENDENCE] is a multiplayer minigames platform where users can compete against each other in real-time. 
@@ -43,38 +45,39 @@ function Home ()
 						</Link>
 					</div>
 				</div>
+
 				{/*Credits*/}
 				<div className="relative w-full py-6 flex flex-col items-center justify-center">
 					<p className="text-lg text-slate-200 opacity-80 font-bold font-aldrich">MADE BY</p>
 					<div className="p-4 gap-2 w-full lg:w-4/5 2xl:w-3/5 flex lg:flex-row flex-wrap justify-around items-center">
-					{/* Change div so it is img + name + role */}
 						<div className="credit-icon">
-							<img src="../public/icon01.jpeg" className="aspect-square rounded-sm" alt=""></img>
+							<img src="/icon01.jpeg" className="w-full aspect-square rounded-sm object-cover" alt="icon of xx on github"></img>
 							<p className="font-aldrich font-bold text-uppercase">NAME</p>
 							<p className="font-aldrich text-neutral-20">role</p>
 						</div>
 						<div className="credit-icon">
-							<img src="/public/icon02.jpeg" className="aspect-square rounded-sm" alt=""></img>
+							<img src="/icon02.jpeg" className="w-full aspect-square rounded-sm object-cover" alt="icon of xx on github"></img>
 							<p className="font-aldrich font-bold text-uppercase">NAME</p>
 							<p className="font-aldrich text-neutral-20">role</p>
 						</div>
 						<div className="credit-icon">
-							<img src="./public/icon03.jpeg" className="aspect-square rounded-sm" alt=""></img>
+							<img src="/icon03.jpeg" className="w-full aspect-square rounded-sm object-cover" alt="icon of xx on github"></img>
 							<p className="font-aldrich font-bold text-uppercase">NAME</p>
 							<p className="font-aldrich text-neutral-20">role</p>
 						</div>
 						<div className="credit-icon">
-							<img src="../../public/icon04.jpeg" className="aspect-square rounded-sm" alt="lol"></img>
+							<img src="/icon04.jpeg" className="w-full aspect-square rounded-sm object-cover" alt="icon of xx on github"></img>
 							<p className="font-aldrich font-bold text-uppercase">NAME</p>
 							<p className="font-aldrich text-neutral-20">role</p>
 						</div>
 						<div className="credit-icon">
-							<img className="aspect-square rounded-sm object-cover" src="public/icon05.jpeg" alt=""></img>
+							<img src="/icon05.jpeg" className="w-full aspect-square rounded-sm object-cover" alt="icon of xx on github"></img>
 							<p className="font-aldrich font-bold text-uppercase">NAME</p>
 							<p className="font-aldrich text-neutral-20">role</p>
 						</div>
 					</div>
-					<Link to="https://github.com/eg-delacruz/42_ft_transcendence">
+					<label htmlFor="giticon" className="labelCustom p-2">see repo on github</label>
+					<Link to="https://github.com/eg-delacruz/42_ft_transcendence" id="giticon">
 						<FontAwesomeIcon icon={faGithub} className="text-white text-4xl"/>
 					</Link>
 				</div>
