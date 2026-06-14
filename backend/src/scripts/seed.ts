348d72b84e2c95b6c30a57c9342d874685733614
Gerardo De La Cruz <netoxas3107@gmail.com>
2026-06-14 12:47:45 +0200

    feat: update /backend/README.md with explanations of how to login as super_admin

--- backend/src/scripts/seed.ts
+++ backend/src/scripts/seed.ts
@@ -1,52 +1,45 @@
-import 'dotenv/config';
-import { connectDB } from '@config/db';
-import env from '@config/env';
-import { logger } from '@config/logger';
-import { User } from '@modules/user/user.model';
-import { hash } from 'bcrypt';
+import "dotenv/config";
+import { connectDB } from "@config/db";
+import env from "@config/env";
+import { logger } from "@config/logger";
+import { User } from "@modules/user/user.model";
+import { hash } from "bcrypt";
 
 export async function ensureSuperUser() {
-    try {
-        // await connectDB();
-        logger.info('Running ensureSuperUser script...');
+  try {
+    // await connectDB();
+    logger.info("Running ensureSuperUser script...");
 
-        // Checks if super user / exisiting user is already created
-        const existingUser = await User.findOne({ email: env.SUPER_EMAIL });
-        if (existingUser) {
-            logger.warn('Supper user already exists, skipping.');
-            return;
-        }
-        else
-            seedSuperUser();
-    }
-    catch (error) {
-        logger.error('Seed failed: ' + error);
-        process.exitCode = 1;
-    }
+    // Checks if super user / exisiting user is already created
+    const existingUser = await User.findOne({ email: env.SUPER_EMAIL });
+    if (existingUser) {
+      logger.warn("Supper user already exists, skipping.");
+      return;
+    } else seedSuperUser();
+  } catch (error) {
+    logger.error("Seed failed: " + error);
+    process.exitCode = 1;
+  }
 }
 
 async function seedSuperUser() {
-    try {
-        
-        // Connect to DB to running this script
-        // await connectDB();
-        logger.info('Running seed script...');
+  try {
+    // Connect to DB to running this script
+    logger.info("Running seed script...");
 
-        // Hash super user password 
-        const hashedPassword = await hash(env.SUPER_PASS, 10);
+    // Hash super user password
+    const hashedPassword = await hash(env.SUPER_PASS, 10);
 
-        // Create the super user in the mongo db
-        await User.create({
-            email: env.SUPER_EMAIL,
-            password: hashedPassword,
-            role: 'super_admin',
-        });
+    // Create the super user in the mongo db
+    await User.create({
+      email: env.SUPER_EMAIL,
+      password: hashedPassword,
+      role: "super_admin",
+    });
 
-        logger.info('Super user created successfully.');
-    }
-    catch(error)
-    {
-        logger.error('Seed failed: ' + error);
-        process.exitCode = 1;
-    }
+    logger.info("Super user created successfully.");
+  } catch (error) {
+    logger.error("Seed failed: " + error);
+    process.exitCode = 1;
+  }
 }
